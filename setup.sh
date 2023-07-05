5d7e99340122bd7d333a71a64b09344c5bd9106e
p3nguin-kun <p3nguinkun@proton.me>
2023-07-05 18:41:24 +0700

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -5,15 +5,20 @@ Gre='\e[0;32m';
 Cya='\e[0;36m';
 Whi='\e[0;37m';
 
+# Check if this script is run as root
+if [ "$(id -u)" = 0 ]; then
+	echo "This script MUST NOT be run as root."
+	exit 1
+fi
 
 # Synchronize package databases
 echo -e "[${Red}*${Whi}] Updating system.."
-sudo pacman -Syu
+sudo pacman -Syu --noconfirm
 
 # Install git
-sudo pacman -S -- git
+sudo pacman -S --noconfirm git
 
-echo -e "[${Red}*${Whi}] clone some git"
+echo -e "[${Red}*${Whi}] Cloning dotfiles"
 mkdir ~/GitGit
 cd GitGit
 git clone https://github.com/jaseg/lolcat.git\ncd lolcat\nmake && sudo make install
@@ -23,22 +28,32 @@ cd
 
 # Install yay 
 echo -e "[${Red}*${Whi}] install yay"
-git clone https://aur.archlinux.org/yay.git && cd yay  && makepkg -si
+if command -v yay &>/dev/null; then
+	echo "Yay is installed in your system"
+else
+	echo "Installing yay"
+	sudo pacman -S --needed --noconfirm base-devel && git clone https://aur.archlinux.org/yay.git && cd yay && makepkg -si --noconfirm && cd ..
+fi
 
 # Install pkgs
 echo -e "[${Red}+${Whi}] Installing repo packages"
 
-for repo_pkg in $(cat  ~/GitGit/dot/.assets/pkg_lists/pkg_list); do 
-  result=$(pacman -Ss \^$repo_pkg\$)
-  
-  if [ -n "$result" ]; then
-    sudo pacman -S $repo_pkg
-  else 
-    yay -S --noconfirm ${repo_pkg}
-  fi
-done
+is_installed() {
+	pacman -Qi "$1" &>/dev/null
+	return $?
+}
 
-# Setup Nvim
+for paquete in "$(cat  ~/GitGit/dot/.assets/pkg_lists/pkg_list)"; do
+	if ! is_installed "$paquete"; then
+		yay -S --noconfirm "$paquete"
+		printf "\n"
+	else
+		printf '%s%s is already installed on your system!%s\n' "${Gre}" "$paquete"
+	fi
+done
+sleep 1
+
+# Setup zsh
 echo -e "[${Gre}*${Whi}] Setting up ZSH plugins"
 
 mkdir -p ~/.config/zsh
@@ -50,9 +65,6 @@ cp -r dot/.config/zsh ~/.config/zsh
 
 bash -c "$(curl --fail --show-error --silent --location https://raw.githubusercontent.com/zdharma-continuum/zinit/HEAD/scripts/install.sh)"
 zinit self-update
-exec zsh
-
-
 
 # Clone GTK theme(s) and icons
 echo -e "[${Gre}*${Whi}] Cloning GTK theme, font and icons"
@@ -72,7 +84,7 @@ elif [[ $ans == "no" || $ans == "NO" || $ans == "n" || $ans == "N" ]]; then
 # Enable 
 
 # LightDm for start your Lap/PC
-echo "[${Gre}*${Whi}] Installing LightDm"
+echo "[${Gre}*${Whi}] Installing LightDM"
 
 yay -S --noconfirm lightdm lightdm-gtk-greeter
 sudo systemctl enable lightdm.service
@@ -84,10 +96,11 @@ yay -S --noconfirm pipewire pipewire-pulse helvum pavucontrol pipewire-alsa
 systemctl --user enable pipewire pipewire-pulse
 systemctl --user start pipewire pipewire-pulse
 
-
-
-
 # Reupdate
 yay -Syu
 sudo chown -R $USER $HOME
+
+# Change shell to zsh
+echo "[${Gre}*${Whi}] Changing shell to zsh"
+chsh -s /usr/bin/zsh
 
